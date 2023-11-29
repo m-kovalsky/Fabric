c773beebeda6a20fb3960b9762cf43197f7de9ec
m-kovalsky <mikova@microsoft.com>
2023-11-29 04:04:58 +0100

    1.0.2

--- OptimizeLakehouseTables.py
+++ OptimizeLakehouseTables.py
@@ -3,10 +3,13 @@
 from delta.tables import *
 
 tables = spark.catalog.listTables()
+tableCount = len(tables)
 
+i = 1
 for t in tables:
     tableName = t.name
     delta_table_path = f"Tables/{tableName}"
     deltaTable = DeltaTable.forPath(spark, delta_table_path)
     deltaTable.optimize().executeCompaction()
-    print("The '" + tableName + "' table has been optimized.")
+    print("The '" + tableName + "' table has been optimized. (" + str(i) + "/" + str(tableCount) + ")")
+    i+=1
