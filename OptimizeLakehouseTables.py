eb4a3fdeb1d36a9b40cd869dc29202545fd537b5
m-kovalsky <mikova@microsoft.com>
2023-11-29 03:51:59 +0100

    1.0.1

--- OptimizeLakehouseTables.py
+++ OptimizeLakehouseTables.py
@@ -1,3 +1,5 @@
+# This code snippet runs the OPTIMIZE function against all the tables in your lakehouse (in order to improve query performance)
+
 from delta.tables import *
 
 tables = spark.catalog.listTables()
