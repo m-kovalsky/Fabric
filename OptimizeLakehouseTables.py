a1e298bc5806f72a2c0b878586d7f2e1d74be373
m-kovalsky <mikova@microsoft.com>
2023-12-05 13:33:52 +0100

    1.0.4

--- OptimizeLakehouseTables.py
+++ OptimizeLakehouseTables.py
@@ -1,13 +1,15 @@
 # This code snippet runs the OPTIMIZE function against all the tables in your lakehouse (in order to improve query performance)
 
 from delta.tables import *
+from tqdm.auto import tqdm
 
 tables = spark.catalog.listTables()
 tableCount = len(tables)
 
-i = 1
-for t in tables:
-    tableName = t.name
+i=1
+for t in (bar := tqdm(tables)):
+    tableName = t.name    
+    bar.set_description(f"Optimizing {tableName}")
     delta_table_path = f"Tables/{tableName}"
     deltaTable = DeltaTable.forPath(spark, delta_table_path)
     deltaTable.optimize().executeCompaction()
