e2ac7f404b84121cda037b60afdbd6df2cf4c67e
m-kovalsky <mikova@microsoft.com>
2024-02-18 11:25:56 +0200

    1.0.0

---
+++ patch_dataset_datasource.py
@@ -0,0 +1,31 @@
+import sempy
+import sempy.fabric as fabric
+import json
+import pandas as pd
+
+# Run this script after running the create_semantic_model script
+
+def patch_dataset_datasource(datasetName):
+
+    dsInfo = get_dataset_datasource(datasetName)
+    gatewayId = dsInfo['Gateway ID'].iloc[0]
+    datasourceId = dsInfo['Datasource ID'].iloc[0]
+
+    gDS = list_gateway_datasource(gatewayId)
+    gDS_filt = gDS[gDS['Data Source ID'] == dsId]
+    credType = gDS_filt['Credential Type'].iloc[0]
+
+    client = fabric.PowerBIRestClient()
+    request_body = {
+    "credentialDetails": {
+        "credentialType": credType,
+        "encryptedConnection": "Encrypted",
+        "encryptionAlgorithm": "None",
+        "privacyLevel": "Organizational",
+        "useCallerAADIdentity": "True"
+    }
+    }
+
+    response = client.patch(f"/v1.0/myorg/gateways/{gatewayId}/datasources/{datasourceId}", json=request_body)
+
+    return response
