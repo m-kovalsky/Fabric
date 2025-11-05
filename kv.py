9bcc2ac97056d7475ee9d920a7bca2d9ee018ae9
m-kovalsky <mikova@microsoft.com>
2025-11-05 08:33:30 +0200

    Update kv.py

--- kv.py
+++ kv.py
@@ -6,9 +6,9 @@ secret_name = ''
 token = notebookutils.credentials.getToken("keyvault")
 headers = {"Authorization": f"Bearer {token}"}
 response = requests.get(
-    f"{key_vault_uri}/secrets/{secret_name}/versions?api-version=7.4", headers=headers,
+    f"{key_vault_uri}/secrets/{secret_name}?api-version=2025-07-01", headers=headers,
 )
 url = response.json().get("value")[-1].get("id")
-response = requests.get(f"{url}?api-version=7.4")
+response = requests.get(f"{url}?api-version=2025-07-01")
 value = response.json().get("value")
 print(value)
